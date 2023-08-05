54ad71722984f131bda5aa2aab26049b069642c3
Khachik001 <114800400+Khachik001@users.noreply.github.com>
2023-08-05 18:43:52 +0400

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -3,6 +3,7 @@ terraform {
     bucket = "terraform-intern-ecr"
     key    = "intern/terraform.tfstate"
     region = "us-east-1"
+    encrypt = true
     dynamodb_table = "terraform-intern-ecr-s3"
   }
 }
